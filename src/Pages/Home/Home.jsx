261a73db8c731f23e7ea5f6dc667ce26c1bc50b7
juanda hernandez <juandahernandez27991@gmail.com>
2021-11-15 14:17:39 -0500

    test2

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,77 +1,64 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
 
 // components
-import WeatherForm from '../../Components/Home/Form/WeatherForm'
-import CityInformation from '../../Components/Home/CityInformation/CityInformation'
-
-
+import WeatherForm from "../../Components/Home/Form/WeatherForm";
+import CityInformation from "../../Components/Home/CityInformation/CityInformation";
 
 // styles
 import "./Home.styles.css";
 
-
 const Home = () => {
+  //state
 
-    //state
+  const [cityInfor, setCityInfor] = useState();
+  const [loader, setLoader] = useState(false);
+  const [errorInput, setErrorInput] = useState(false);
+  const [valueInput] = useState(null);
+  const [cityName, setCityName] = useState("");
 
+  // funciones
 
-    const [cityInfor, setCityInfor] = useState()
-    const [loader, setLoader] = useState(false);
-    const [errorInput, setErrorInput] = useState(false)
-    const [valueInput] = useState(null)
-    const [cityName, setCityName] = useState("");
+  const buscarCiudad = ({ value }) => {
+    setCityName(value);
+  };
 
-    // funciones
-
-
-    const buscarCiudad = ({ value }) => {
-        setCityName(value);
-    };
-
-    const buscarClima = async (e) => {
-        e.preventDefault()
-        setCityName(valueInput)
-        if (!cityName) {
-            setErrorInput(true)
-            setCityInfor(null)
-
-
-        } else {
-            setErrorInput(false)
-            const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
-            const response = await fetch(API);
-            const result = await response.json();
-            setCityInfor(result);
-            setLoader(false)
-
-        }
+  const buscarClima = async (e) => {
+    e.preventDefault();
+    setCityName(valueInput);
+    if (!cityName) {
+      setErrorInput(true);
+      setCityInfor(null);
+    } else {
+      setErrorInput(false);
+      const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
+      const response = await fetch(API);
+      const result = await response.json();
+      setCityInfor(result);
+      setLoader(false);
     }
+  };
 
-
-    return (
-        <div className="container">
-
-
-            <div className="background">
-                <WeatherForm
-                    buscarCiudad={buscarCiudad}
-                    cityName={cityName}
-                    buscarClima={buscarClima}
-                    errorInput={errorInput}
-                />
-                {
-
-                    cityInfor ?
-                        cityInfor.cod === "404" ? <p className="error404">{cityInfor.message}</p> :
-                            (<CityInformation
-                                cityInfor={cityInfor}
-                            />) : null
-
-                }
-            </div>
+  return (
+    <div className="container">
+      {!loader && (
+        <div className="background">
+          <WeatherForm
+            buscarCiudad={buscarCiudad}
+            cityName={cityName}
+            buscarClima={buscarClima}
+            errorInput={errorInput}
+          />
+          {cityInfor ? (
+            cityInfor.cod === "404" ? (
+              <p className="error404">{cityInfor.message}</p>
+            ) : (
+              <CityInformation cityInfor={cityInfor} />
+            )
+          ) : null}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
